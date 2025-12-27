c87cef6c1ae1d5cbca35bfefce8c75553c2364d9
mleaf233 <magicleaf06151@gmail.com>
2025-12-27 13:36:28 +0800

    add localization for blueprint

---
+++ impl/mods/blueprint/localization/zh_CN.lua
@@ -0,0 +1,12 @@
+return {
+    translator = {
+        "mleaf233",
+    },
+    misc = {
+        dictionary = {
+            testtesttest = "hi hello hi",
+            blueprint_config = "动态 蓝图",
+            brainstorm_config = "动态 头脑风暴",
+        }
+    }
+}
