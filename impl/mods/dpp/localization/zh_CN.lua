e56d8ecde3edb18b12d057989d6342a4539ac8c9
mleaf233 <magicleaf06151@gmail.com>
2025-12-31 22:25:10 +0800

    add localization for Dubugplusplus mod

---
+++ impl/mods/dpp/localization/zh_CN.lua
@@ -0,0 +1,99 @@
+return {
+    misc = {
+        thanslator = {
+            "mleaf233",
+        },
+        dictionary = {
+            dpp_name = "调试面板++",
+            dpp_open = "打开调试面板++",
+            dpp_close = "关闭调试面板++",
+
+            -- 配置标签
+            dpp_meta_label = "模组",
+            dpp_meta_background_colour = "背景颜色",
+            dpp_meta_menu_disclaimer = "（或按[TAB]键打开菜单）",
+            dpp_meta_menu_button_toggle = "添加调试面板++暂停菜单按钮",
+            dpp_meta_require_ctrl = "需要同时按住Ctrl键来切换菜单",
+            dpp_meta_open_og_debug = "打开原始调试菜单",
+            dpp_meta_override_overlay = "覆盖叠加菜单",
+
+            -- 制作人员标签
+            dpp_credits_label = "制作人员",
+            dpp_credits_localization_label = "本地化",
+            dpp_credits_special_thanks_label = "特别感谢",
+
+            -- 语言
+            ["dpp_lan_en-us"] = "英文",
+            ["dpp_lan_es_ES"] = "西班牙文",
+            ["dpp_lan_zh_CN"] = "中文",
+
+            -- 名字
+            ["dpp_names_Jogla"] = "Jogla",
+            ["dpp_names_GoldenLeaf"] = "GoldenLeaf（？？？没看到有原mod的中文本地化）",
+            ["dpp_names_bepisfever"] = "BepisFever",
+
+            -- 通用
+            dpp_gen_apply = "应用",
+            dpp_gen_remove = "移除",
+            dpp_gen_mod = "修改",
+            dpp_gen_set = "设置",
+
+            -- 卡牌标签
+            dpp_card_label = "卡牌",
+            dpp_card_disclaimer = {
+                "（应用到",
+                "选中的卡牌）"
+            },
+            dpp_card_rank = "点数",
+            dpp_card_suit = "花色",
+            dpp_card_enhancement = "增强",
+            dpp_card_edition = "版本",
+            dpp_card_seal = "蜡封",
+            dpp_card_sticker = "贴纸",
+
+            -- 玩家
+            dpp_player_label = "玩家",
+            dpp_player_hand_label = "手牌",
+            dpp_player_hands_label = "出牌次数",
+            dpp_player_discards_label = "弃牌次数",
+            dpp_player_draw_hand = '抽牌',
+            dpp_player_unlimited = "无限",
+            dpp_player_jokers_label = "小丑牌",
+            dpp_player_consumable_label = "消耗牌",
+            dpp_player_money = "资金",
+            dpp_player_slots = "槽位",
+            dpp_player_highlight = "选择上限",
+
+            -- 对局标签
+            dpp_run_label = "对局",
+            dpp_run_blind_label = "盲注",
+            dpp_run_set_blind = "设置Boss盲注",
+            dpp_run_win_blind = "击败盲注",
+            dpp_run_player_chips = "玩家筹码",
+            dpp_run_blind_chips = "所需筹码",
+            dpp_run_ante_label = "底注",
+            dpp_run_round_label = "回合",
+            dpp_run_win_label = "赢得对局",
+            dpp_run_lose_label = "输掉对局",
+            dpp_run_modify_poker_hands_label = '修改牌型',
+            dpp_run_dismiss = "解散",
+            dpp_run_disclaimer = "由调试面板++修改",
+
+            -- 游戏标签
+            dpp_game_label = "游戏",
+            dpp_game_speed_label = "速度",
+
+            -- 存档状态
+            dpp_savestate_label = '存档状态',
+            dpp_savestate_save = "保存",
+            dpp_savestate_load = "加载",
+            dpp_savestate_delete = "删除",
+            dpp_savestate_new_label = '新存档状态',
+            dpp_savestate_no_savestates_label = {
+                "暂无存档状态",
+                "要创建存档状态，请在文本输入框中",
+                "输入名称并选择‘保存’"
+            }
+        }
+    }
+}
